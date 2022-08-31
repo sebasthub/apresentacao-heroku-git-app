feb43e0036d707c31e38298c64cf8b1bc99c13c7
sebasthub <playandroidhb@gmail.com>
2022-08-31 18:47:01 -0300

    navbar provisoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaviBarComponent } from './components/navi-bar/navi-bar.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaviBarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
